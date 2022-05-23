970d452f1350f2442b4209574443b5ee8fa0a57c
leoD-E1 <leodangeli11@gmail.com>
2022-05-23 03:21:36 -0300

    [implementation] addded panel of the desplegable menu, missing the content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,24 +13,26 @@ interface CardElement {
 const Card = (obj: CardElement) => {
   return (
     <div className="group relative">
-      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <img
-          src={obj.imageSrc}
-          alt={obj.imageAlt}
-          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-        />
-      </div>
-      <div className="mt-4 flex justify-between bg-slate-400">
-        <div>
-          <h3 className="text-sm text-gray-700">
-            <a href={obj.href}>
-              <span aria-hidden="true" className="absolute inset-0" />
-              {obj.name}
-            </a>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{obj.color}</p>
+      <div className="group-hover:opacity-90 group-hover:shadow-lg group-hover:shadow-orange-700/50">
+        <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-80 lg:aspect-none">
+          <img
+            src={obj.imageSrc}
+            alt={obj.imageAlt}
+            className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+          />
         </div>
-        <p className="text-sm font-medium text-gray-900">{obj.price}</p>
+        <p className="text-xl font-extrabold text-gray-900">{obj.price}</p>
+        <div className="my-2 flex justify-center">
+          <div>
+            <h3 className="text-md text-gray-700">
+              <a href={obj.href}>
+                <span aria-hidden="true" className="absolute inset-0" />
+                {obj.name}
+              </a>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{obj.color}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import {
   HiMenu as MenuIcon,
   HiSearch as SearchIcon,
   HiShoppingBag as ShoppingBagIcon,
+  HiX as XIcon,
 } from "react-icons/hi";
 
 const navigation = {
@@ -130,6 +131,8 @@ const navigation = {
     },
   ],
   pages: [
+    { name: "link1", href: "#" },
+    { name: "link2", href: "#" },
     { name: "Company", href: "#" },
     { name: "Stores", href: "#" },
   ],
@@ -139,104 +142,121 @@ const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <header>
-      <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="border-b border-gray-200">
-          <div className="h-16 flex items-center">
-            <button
-              type="button"
-              className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
-              onClick={() => setOpen(true)}
-            >
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+    <>
+      {!isOpen ? null : (
+        <>
+          <div className="relative z-40 lg:hidden">
+            <div className="fixed h-full max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+              <div className="px-4 pt-5 pb-2 flex">
+                <button
+                  type="button"
+                  className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                  onClick={() => setOpen(false)}
+                >
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </>
+      )}
+      <header>
+        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="border-b border-gray-200">
+            <div className="h-16 flex items-center">
+              <button
+                type="button"
+                className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+                onClick={() => setOpen(true)}
+              >
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
 
-            {/* Logo */}
-            <div className="ml-4 flex lg:ml-0">
-              <a href="#">
-                <span className="">LeodaShop</span>
-                {/* <img
+              {/* Logo */}
+              <div className="ml-4 flex lg:ml-0">
+                <a href="#">
+                  <span>Leoda shop</span>
+                  {/* <img
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
                   alt=""
                 /> */}
-              </a>
-            </div>
+                </a>
+              </div>
 
-            {/* Flyout menus */}
-            <div className="hidden lg:ml-8 lg:block lg:self-stretch">
-              <div className="h-full flex space-x-8">
-                {navigation.pages.map((page) => (
+              {/* Flyout menus */}
+              <div className="hidden lg:ml-8 lg:block lg:self-stretch">
+                <div className="h-full flex space-x-8">
+                  {navigation.pages.map((page) => (
+                    <a
+                      key={page.name}
+                      href={page.href}
+                      className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
+                    >
+                      {page.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   <a
-                    key={page.name}
-                    href={page.href}
-                    className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
+                    href="#"
+                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
                   >
-                    {page.name}
+                    Sign in
                   </a>
-                ))}
-              </div>
-            </div>
+                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                  >
+                    Create account
+                  </a>
+                </div>
 
-            <div className="ml-auto flex items-center">
-              <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                <a
-                  href="#"
-                  className="text-sm font-medium text-gray-700 hover:text-gray-800"
-                >
-                  Sign in
-                </a>
-                <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                <a
-                  href="#"
-                  className="text-sm font-medium text-gray-700 hover:text-gray-800"
-                >
-                  Create account
-                </a>
-              </div>
+                <div className="hidden lg:ml-8 lg:flex">
+                  <a
+                    href="#"
+                    className="text-gray-700 hover:text-gray-800 flex items-center"
+                  >
+                    <img
+                      src="https://tailwindui.com/img/flags/flag-canada.svg"
+                      className="w-5 h-auto block flex-shrink-0"
+                    />
+                    <span className="ml-3 block text-sm font-medium">CAD</span>
+                  </a>
+                </div>
 
-              <div className="hidden lg:ml-8 lg:flex">
-                <a
-                  href="#"
-                  className="text-gray-700 hover:text-gray-800 flex items-center"
-                >
-                  <img
-                    src="https://tailwindui.com/img/flags/flag-canada.svg"
-                    alt=""
-                    className="w-5 h-auto block flex-shrink-0"
-                  />
-                  <span className="ml-3 block text-sm font-medium">CAD</span>
-                  <span className="sr-only">, change currency</span>
-                </a>
-              </div>
+                {/* Search */}
+                <div className="flex lg:ml-6">
+                  <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Search</span>
+                    <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                  </a>
+                </div>
 
-              {/* Search */}
-              <div className="flex lg:ml-6">
-                <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
-                  <span className="sr-only">Search</span>
-                  <SearchIcon className="w-6 h-6" aria-hidden="true" />
-                </a>
-              </div>
-
-              {/* Cart */}
-              <div className="ml-4 flow-root lg:ml-6">
-                <a href="#" className="group -m-2 p-2 flex items-center">
-                  <ShoppingBagIcon
-                    className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                    aria-hidden="true"
-                  />
-                  <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                    0
-                  </span>
-                  <span className="sr-only">items in cart, view bag</span>
-                </a>
+                {/* Cart */}
+                <div className="ml-4 flow-root lg:ml-6">
+                  <a href="#" className="group -m-2 p-2 flex items-center">
+                    <ShoppingBagIcon
+                      className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                      0
+                    </span>
+                    <span className="sr-only">items in cart, view bag</span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 };
 
