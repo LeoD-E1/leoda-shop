d209e53a44c2abac4a37953b4a67e5c915002532
leoD-E1 <leodangeli11@gmail.com>
2022-05-23 23:09:23 -0300

    added little things of style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ interface CardElement {
 const Card = (obj: CardElement) => {
   return (
     <div className="group relative">
-      <div className="group-hover:opacity-90 group-hover:shadow-lg group-hover:shadow-orange-700/50">
+      <div className="border-2 border-transparent group-hover:opacity-90 group-hover:shadow-lg group-hover:border-solid group-hover:border-sky-700 rounded-md">
         <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-80 lg:aspect-none">
           <img
             src={obj.imageSrc}
@@ -21,7 +21,7 @@ const Card = (obj: CardElement) => {
             className="w-full h-full object-center object-cover lg:w-full lg:h-full"
           />
         </div>
-        <p className="text-xl font-extrabold text-gray-900">{obj.price}</p>
+        <p className="my-2 text-xl font-extrabold text-gray-900">{obj.price}</p>
         <div className="my-2 flex justify-center">
           <div>
             <h3 className="text-md text-gray-700">
@@ -30,7 +30,7 @@ const Card = (obj: CardElement) => {
                 {obj.name}
               </a>
             </h3>
-            <p className="mt-1 text-sm text-gray-500">{obj.color}</p>
+            <p className="my-2 text-sm text-gray-500">{obj.color}</p>
           </div>
         </div>
       </div>
--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -7,13 +7,13 @@ const Menu = (prosp: any) => {
     <>
       <div className="relative z-40 lg:hidden">
         <div className="fixed h-full max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
-          <div className="px-4 pt-3 pb-2 flex">
+          <div className="px-4 pt-4 pb-2 flex">
             <button
               type="button"
               className="m-1 p-1 rounded-full inline-flex items-center justify-center text-gray-600 hover:bg-gray-200 focus:bg-gray-300"
               onClick={() => setOpen(false)}
             >
-              <XIcon className="h-6 w-6 " aria-hidden="true" />
+              <XIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
 
@@ -22,7 +22,7 @@ const Menu = (prosp: any) => {
               <div key={page.name} className="flow-root">
                 <a
                   href={page.href}
-                  className="-m-2 p-2 block font-medium text-gray-900"
+                  className="-m-2 p-2 block font-medium text-gray-900 hover:bg-gray-300 focus:bg-gray-400"
                 >
                   {page.name}
                 </a>
@@ -36,24 +36,26 @@ const Menu = (prosp: any) => {
                 Sign in
               </a>
             </div>
+
             <div className="flow-root">
               <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
                 Create account
               </a>
             </div>
-            <div className="border-t border-gray-200 py-6 px-4">
-              <a href="#" className="-m-2 p-2 flex items-center">
-                <img
-                  src="https://tailwindui.com/img/flags/flag-canada.svg"
-                  alt=""
-                  className="w-5 h-auto block flex-shrink-0"
-                />
-                <span className="ml-3 block text-base font-medium text-gray-900">
-                  CAD
-                </span>
-                <span className="sr-only">, change currency</span>
-              </a>
-            </div>
+          </div>
+
+          <div className="border-t border-gray-200 py-6 px-4">
+            <a href="#" className="-m-2 p-2 flex items-center">
+              <img
+                src="https://tailwindui.com/img/flags/flag-canada.svg"
+                alt=""
+                className="w-5 h-auto block flex-shrink-0"
+              />
+              <span className="ml-3 block text-base font-medium text-gray-900">
+                CAD
+              </span>
+              <span className="sr-only">, change currency</span>
+            </a>
           </div>
         </div>
       </div>
