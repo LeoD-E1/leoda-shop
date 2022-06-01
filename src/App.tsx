2ef72a943069aa707b1b554ef039d965430a2814
leoD-E1 <leodangeli11@gmail.com>
2022-05-31 23:22:38 -0300

    [update] added functions for connect with backend, removed data mocked in home for pass as prop in home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./style/App.css";
 import Home from "./pages/Visitor/Home";
+import Navbar from "./components/Navbar/Navbar";
+import { Switch, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/:user/home" component={Home} />
+        {/* <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={DashboardView} />
+        <Route path="/customers" component={CustomersView} /> */}
+        {/*  Routes of Orders */}
+        <Route path="/orders" /* component={Orders } */ />
+        <Route path="/orders/:order" /* component={ DetailedOrder } */ />
+      </Switch>
     </div>
   );
 }
--- src/pages/Visitor/Home.tsx
+++ src/pages/Visitor/Home.tsx
@@ -1,55 +1,23 @@
 import Navbar from "../../components/Navbar/Navbar";
 import CardGroup from "../../components/CardGroup";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const products = [
-    {
-      id: 1,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: "$35",
-      color: "Black",
-    },
-    {
-      id: 2,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: "$35",
-      color: "Black",
-    },
-    {
-      id: 3,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-03.jpg",
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: "$35",
-      color: "Black",
-    },
-    {
-      id: 4,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-03.jpg",
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: "$35",
-      color: "Black",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await fetch("http://127.0.0.1:4000/api/v1/products/");
+    const data = await response.json();
+    setProducts(data.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   const title = "Customers also purchased";
 
   return (
     <>
-      <Navbar />
       <CardGroup title={title} arrObj={[...products]} />
     </>
   );
